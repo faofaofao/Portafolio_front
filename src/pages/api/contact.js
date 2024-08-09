6867b17c28d9265b7ce0a5d4df146f6b43a8f09a
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-09 14:35:49 -0400

    config api

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,14 +1,14 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export default async function handler(req, res) {
-//   if (req.method === 'POST') {
-//     try {
-//       const response = await axios.post(`${process.env.BACK_API_URL}/api/contact`, req.body);
-//       res.status(response.status).json(response.data);
-//     } catch (error) {
-//       res.status(error.response ? error.response.status : 500).json({ message: 'Error al enviar el mensaje', error: error.message });
-//     }
-//   } else {
-//     res.status(405).json({ message: 'Método no permitido' });
-//   }
-// }
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const response = await axios.post(`${process.env.BACK_API_URL}/api/contact`, req.body);
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      res.status(error.response ? error.response.status : 500).json({ message: 'Error al enviar el mensaje', error: error.message });
+    }
+  } else {
+    res.status(405).json({ message: 'Método no permitido' });
+  }
+}
